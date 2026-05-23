84e8d7f63f2666767f5ce28d4b18e454487592ca
Fey Xie <feyxiexzf@gmail.com>
2026-05-23 23:55:39 +0800

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# wauxv_deobf_new
+
+[新版本](https://github.com/HdShare/WAuxiliary_Public/releases/tag/1356-1.2.7.r1356.3e63484)的反混淆, 微调了一些 jadx 参数, 优化了字符串解密脚本
+
+我尚未且不打算 (我懒) 把[旧版](https://github.com/Ujhhgtg/wauxv_deobf)的类名和方法名映射迁移过来, 没什么意义, 混淆代码看多了自然就知道是啥意思, 实在不行两边对照着看也行
--- app/src/main/java/p000/C1039.java
+++ app/src/main/java/p000/C1039.java
@@ -54,7 +54,6 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return AbstractC0743.m2186((String) obj2).getAbsolutePath();
             case 5:
                 Object obj3 = objArr[0];
-                String[] strArr = AbstractC1574.f5469;
                 Object obj4 = objArr[1];
                 File fileM2186 = AbstractC0743.m2186((String) obj3);
                 AbstractC1328.m3042(fileM2186, new File((String) obj4));
@@ -68,7 +67,6 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return AbstractC1459.m3196(new File((String) obj6)).getAbsolutePath();
             case 8:
                 Object obj7 = objArr[0];
-                String[] strArr2 = AbstractC1574.f5469;
                 Object obj8 = objArr[1];
                 File fileM3196 = AbstractC1459.m3196(new File((String) obj7));
                 AbstractC1328.m3042(fileM3196, new File((String) obj8));
@@ -82,7 +80,6 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return C0774.getSelfWxId();
             case 11:
                 Object obj10 = objArr[0];
-                String[] strArr3 = AbstractC1574.f5469;
                 Object obj11 = objArr[1];
                 
                 C2180.addNetSceneToQueue(AbstractC1574.dexToCtor(C2161.f7121).newInstance((String) obj10, (List) obj11, 0));
@@ -90,20 +87,17 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
             case 12 /* 12 */:
                 
                 Object objInvoke = AbstractC1574.dexToMethod(C0773.f2873).invoke(null, null);
-                String[] strArr4 = AbstractC1574.f5469;
                 Object obj12 = ((Map) objInvoke).get("last_login_alias");
                 String str = obj12 instanceof String ? (String) obj12 : null;
                 return str == null ? C0774.getSelfWxId() : str;
             case 13:
                 Object obj13 = objArr[0];
-                String[] strArr5 = AbstractC1574.f5469;
                 Object obj14 = objArr[1];
                 Object obj15 = objArr[2];
                 C1133.m2727(((Integer) obj15).intValue(), (String) obj13, (String) obj14, 0);
                 return unit;
             case 14 /* 14 */:
                 Object obj16 = objArr[0];
-                String[] strArr6 = AbstractC1574.f5469;
                 Object obj17 = objArr[1];
                 Object obj18 = objArr[2];
                 int iIntValue = ((Integer) obj18).intValue();
@@ -112,13 +106,11 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return unit;
             case 15:
                 Object obj20 = objArr[0];
-                String[] strArr7 = AbstractC1574.f5469;
                 Object obj21 = objArr[1];
                 C1133.m2713((String) obj20, Collections.singletonList((String) obj21));
                 return unit;
             case 16:
                 Object obj22 = objArr[0];
-                String[] strArr8 = AbstractC1574.f5469;
                 Object obj23 = objArr[1];
                 C1133.m2713((String) obj22, (List) obj23);
                 return unit;
@@ -134,8 +126,7 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
             case 20:
                 Object obj25 = objArr[0];
                 String str2 = (String) obj25;
-                C0819 c0819 = C0819.f3207;
-                
+
                 String conRemark = new ContactBean(C0819.m2371(str2)).getConRemark();
                 if (!AbstractC2841.m4836(conRemark)) {
                     return conRemark;
@@ -144,14 +135,12 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return new ContactBean(C0819.m2371(str2)).getNickname();
             case 21 /* 21 */:
                 Object obj26 = objArr[0];
-                String[] strArr9 = AbstractC1574.f5469;
                 Object obj27 = objArr[1];
                 boolean zBooleanValue = ((Boolean) obj27).booleanValue();
                 
                 return C1873.m3654((String) obj26, zBooleanValue);
             case 22 /* 22 */:
                 Object obj28 = objArr[0];
-                String[] strArr10 = AbstractC1574.f5469;
                 String str3 = (String) obj28;
                 Object obj29 = objArr[1];
                 
@@ -160,7 +149,6 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                     
                     Object objInvoke2 = AbstractC1574.dexToMethod(C0651.f2503).invoke(objM2066, str3);
                     if (objInvoke2 != null) {
-                        int i2 = 0;
                         C1299 c1299M3490 = AbstractC0968.startMethodResolution_(objInvoke2).m3490();
                         c1299M3490.name = "e";
                         String str4 = (String) ((C1315) AbstractC0744.firstInList(c1299M3490.resolve())).m3024();
@@ -171,14 +159,10 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return new ContactBean(C0819.m2371(str3)).getNickname();
             case 23 /* 23 */:
                 Object obj30 = objArr[0];
-                String[] strArr11 = AbstractC1574.f5469;
-                C0669 c0669 = C0669.f2550;
-                String str5 = "";
-                
+
                 return C0669.m2063((String) obj30, "");
             case 24 /* 24 */:
                 Object obj31 = objArr[0];
-                String[] strArr12 = AbstractC1574.f5469;
                 Object obj32 = objArr[1];
                 
                 return C0669.m2063((String) obj31, (String) obj32);
@@ -190,17 +174,14 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return AbstractC2203.m4032();
             case 27:
                 Object obj34 = objArr[0];
-                
-                int i3 = 0;
-                
+
                 MethodResolver c1973M3492 = AbstractC0968.startMethodResolution_(C0672.m2065()).m3492();
                 c1973M3492.returnType = AbstractC2519.classToKClass(List.class);
                 List list = (List) ((C1982) AbstractC2784.setParamsAndResolveFirstMethod(new Object[]{AbstractC2519.classToKClass(String.class)}, 1, c1973M3492)).invoke((String) obj34);
                 return list == null ? C1189.f4329 : list;
             case 28:
                 Object obj35 = objArr[0];
-                String[] strArr13 = AbstractC1574.f5469;
-                
+
                 Method methodM3315 = AbstractC1574.dexToMethod(C0673.f2556);
                 
                 Object objInvoke3 = methodM3315.invoke(C0672.m2065(), (String) obj35);
@@ -209,7 +190,6 @@ public final /* synthetic */ class C1039 implements InterfaceC1467, InterfaceC05
                 return num;
             default:
                 Object obj36 = objArr[0];
-                String[] strArr14 = AbstractC1574.f5469;
                 Object obj37 = objArr[1];
                 List listSingletonList = Collections.singletonList((String) obj37);
                 
